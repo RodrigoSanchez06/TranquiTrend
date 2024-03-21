1cef9841ccc674eb8ac1cec460583fb2a217169a
Rodrigo <rogerxdgta@ciencias.unam.mx>
2024-03-21 10:36:00 -0600

    Termine practica 7

--- src/app/module/product/product.module.ts
+++ src/app/module/product/product.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoryComponent } from './component/category/category.component';
-
+import { FormsModule} from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -9,7 +10,9 @@ import { CategoryComponent } from './component/category/category.component';
     CategoryComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class ProductModule { }
