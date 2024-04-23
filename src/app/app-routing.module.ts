e7254f3216ac24cdc004ace52c488a97139b709d
Rodrigo <rogerxdgta@ciencias.unam.mx>
2024-04-23 12:00:41 -0600

    Practica 8 corregida

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CategoryComponent } from './module/product/component/category/category.component';
+import { RegisterComponent } from './module/authentication/register/register.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
-const routes: Routes = [{path: 'category', component: CategoryComponent}];
+import { LoginComponent } from './module/authentication/login/login.component';
+
+import { SecuredComponent } from './module/authentication/secured/secured.component';
+
+import { authenticationGuard } from './module/authentication/_guard/authentication.guard';
+
+const routes: Routes = [
+  { path: '', component: NavbarComponent, children: [
+    { path: 'category', component: CategoryComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'login', component: LoginComponent },
+    { path: 'secured', component: SecuredComponent, canActivate: [authenticationGuard] }
+  
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/module/product/_service/category.service.ts
+++ src/app/module/product/_service/category.service.ts
@@ -1,18 +1,52 @@
 import { Injectable } from '@angular/core';
-import { Category } from '../_model/Category';
+import { Category } from '../_model/category';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { api_dwb_uri } from '../../../shared/uri/api-dwb-uri';
+import { ApiResponse } from '../../commons/_dto/api-response';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoryService {
 
-  constructor() { }
-  getCategories(): Category[] {
-    let categories: Category[] = [
-      new Category(1, 'Juguetes Antiestres', 'ESTR', 1),
-      new Category(2, 'Aromatizantes', 'CLOTH', 1),
-      new Category(3, 'Productos Naturales', 'FOOT', 1),
-    ];
-    return categories;
+  private source = '/category'
+
+  constructor(private http: HttpClient) { }
+
+  createCategory(category: any): Observable<HttpResponse<ApiResponse>> {
+    return this.http.post<ApiResponse>(api_dwb_uri + this.source, category, { observe: 'response' });
+
   }
+
+  disableCategory(id: number): Observable<HttpResponse<ApiResponse>> {
+    return this.http.delete<ApiResponse>(api_dwb_uri + this.source + '/' + id, { observe: 'response' });
+
+  }
+
+  enableCategory(id: number): Observable<HttpResponse<ApiResponse>> {
+    return this.http.put<ApiResponse>(api_dwb_uri + this.source + '/' + id + "/activate", null, { observe: 'response' });
+  }
+
+  getCategory(id: number): Observable<HttpResponse<Category>> {
+    return this.http.get<Category>(api_dwb_uri + this.source + "/" + id, { observe: 'response' });
+
+
+  }
+
+  getCategories(): Observable<HttpResponse<Category[]>> {
+
+    return this.http.get<Category[]>(api_dwb_uri + this.source, { observe: 'response' });
+  }
+
+  getActiveCategories(): Observable<HttpResponse<Category[]>> {
+    return this.http.get<Category[]>(api_dwb_uri + this.source + "/active", { observe: 'response' });
+  }
+
+  updateCategory(category: any, id: number): Observable<HttpResponse<ApiResponse>> {
+    return this.http.put<ApiResponse>(api_dwb_uri + this.source + "/" + id, category, { observe: 'response' });
+  }
+
+
 }
